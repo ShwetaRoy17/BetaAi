07d1ab73f8cb5bb175ec34f2a553c54ac7eadc3f
shweta roy <128317014+ShwetaRoy17@users.noreply.github.com>
2024-03-23 00:59:24 +0530

    footer modified

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="text-center py-4 absolute bottom-[10px] w-full">
+    <footer className="text-center py-4  mt-[60px] mb-[100px] w-full">
     <p className="text-black leading-[
 21.78px] font-serif font-extrabold">© Copyright BETAI</p>
-  </div>
+  </footer>
   )
 }
 
