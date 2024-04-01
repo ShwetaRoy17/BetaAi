e7c0d9eb1c7ae10dd7b6bad367f55dd0e96d909e
shweta roy <128317014+ShwetaRoy17@users.noreply.github.com>
2024-04-02 00:34:12 +0530

    controllers for country and league list added

--- backend/src/controllers/top.controller.js
+++ backend/src/controllers/top.controller.js
@@ -24,11 +24,45 @@ const getTopLeagues = asyncHandler(async(req,res)=>{
         throw new ApiError(505,"couldn't fetch data");
         
     }
- console.log("data for top league is",data);
+//  console.log("data for top league is",data);
 
     return res
     .status(201)
     .json(new ApiResponse(200,data.data,"top countries data"));
 })
 
-export  {getTopCountries,getTopLeagues};
+
+// player details 
+const getPlayerdetails = asyncHandler(async(req,res)=>{
+    const id = req.params.id;
+    const url = `https://api.football-data-api.com/player-stats?key=${process.env.API_KEY}&player_id=${id}`
+    const data = await fetchData(url);
+    if(!data) {
+        throw new ApiError(500,"Players detail got something wrong");
+
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(201,data,"players detail"))
+})
+
+
+
+
+// team details
+const getTeamdetails = asyncHandler(async(req,res)=>{
+    const id = req.params.id;
+    console.log("id:",id);
+    const url = `https://api.football-data-api.com/team?key=dcf0fc31fba7156ec14dcb521dbfa5ef3099b17ed993a03e11d70c65c24ab30a&team_id=${id}`
+    console.log("url:",url);
+    const data = await fetchData(url);
+    if(!data) {
+        throw new ApiError(500,"team details got something wrong");
+
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(201,data,"team details"))
+})
+
+export  {getTopCountries,getTopLeagues,getPlayerdetails,getTeamdetails};
--- backend/src/routes/top.route.js
+++ backend/src/routes/top.route.js
@@ -1,8 +1,10 @@
 import {Router} from "express"
-import {getTopCountries,getTopLeagues} from '../controllers/top.controller.js'
+import {getTopCountries,getTopLeagues,getPlayerdetails,getTeamdetails} from '../controllers/top.controller.js'
 const router = Router();
 
 router.route("/topcountry").get(getTopCountries)
 router.route("/topleague").get(getTopLeagues);
+router.route("/player/:id").get(getPlayerdetails);
+router.route("/team/:id").get(getTeamdetails)
 
 export default router
