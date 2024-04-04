5f88adb38fb66a7922a45797b0725514db3f24fd
shweta roy <128317014+ShwetaRoy17@users.noreply.github.com>
2024-04-04 16:35:32 +0530

    fixture routes addded

--- backend/src/routes/fixture.route.js
+++ backend/src/routes/fixture.route.js
@@ -1,9 +1,16 @@
 import {Router} from "express"
+import { getTodaysMatch,getMatchDetails,getLastXstats } from "../controllers/fixture.controllers.js";
+import {getLeagueMatches,getLeagueTeams,getLeaguePlayers,getLeagueStats} from '../controllers/league.controllers.js'
+
 
 const router = Router();
 
-router.route("/fixture").get(()=>{
-    return ("fixture")
-})
+router.route("/matches").get(getTodaysMatch);
+router.route("/MatchDetails/:season_id").get(getMatchDetails);
+router.route("/getLastXStats/:team_id&:x").get(getLastXstats);
+router.route("/leagueMatches/:season_id").get(getLeagueMatches);
+router.route("/leagueTeams/:season_id").get(getLeagueTeams);
+router.route("/leaguePlayers/season_id").get(getLeaguePlayers);
+router.route("/leagueStats/:season_id").get(getLeagueStats);
 
 export default router
