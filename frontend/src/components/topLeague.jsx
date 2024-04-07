7b34fed87f1bb6b9fbf79c7ef44340ab083e71f3
shweta roy <128317014+ShwetaRoy17@users.noreply.github.com>
2024-04-08 01:19:15 +0530

    league connected to new url

--- frontend/src/components/topLeague.jsx
+++ frontend/src/components/topLeague.jsx
@@ -1,25 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getTopLeagues } from '../features/TopLeagueSlice.js';// Import Axios
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const TopLeagues = () => {
-  const dispatch = useDispatch();
+  const [leagues,setLeagueList]= useState([]);
   useEffect(() => {
-    dispatch(getTopLeagues());
+    async function help(){
+      const data = await axios.get("http://localhost:8000/api/v1/leagues/topleague");
+      if(data){
+        // console.log("data",data);
+      setLeagueList(data.data.data);
+      }
+    }
+    help();
   }
-  , [dispatch]);
+  , []);
   
-  const leagues = useSelector((state)=>state.topLeague.leagues)
+  
   // console.log("leagues",leagues);
 
   return (
-    <div className="bg-[#1C2632] dark:bg-gray-800 rounded-lg shadow-md py-4 px-2 overflow-y-auto h-full">
-      <h2 className="text-[17px] font-serif text-[#D2D4D6] font-medium mb-4">Top Leagues</h2>
+    <div className="bg-[#1C2632] dark:bg-gray-800 rounded-lg shadow-md py-4 px-1 md:px-2 overflow-y-auto h-full overflow-x-hidden">
+      <h2 className="text-[14px] md:text-[17px] font-serif text-[#D2D4D6] font-medium mb-1 md:mb-4">Top Leagues</h2>
       <ul className="space-y-2">
         {leagues.map((league) => (
-          <li key={league.name} className="flex items-center">
+          <li key={league.name} >
+          <Link to="/home" className="flex items-center">
             <img src={league.image} alt={league.country} className="w-[4vh] h-[4vh] mr-2 rounded-full" />
-            <span className="text-[14px] text-[#D2D4D6] font-normal font-serif">{league.name}</span>
+            <span className="text-[12px] md:text-[14px] text-[#D2D4D6] font-normal font-serif">{league.name}</span>
+          </Link>
           </li>
         ))}
       </ul>
