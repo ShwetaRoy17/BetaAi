fdb5d10d1a787c06ca587580981666373c3a4769
shweta roy <128317014+ShwetaRoy17@users.noreply.github.com>
2024-04-08 01:25:10 +0530

    format date function for unixformat added

--- frontend/src/utils/dictionary.js
+++ frontend/src/utils/dictionary.js
@@ -6,6 +6,19 @@ const Teamlist = {
     
 
 }
+
+export const formatDate = (unixTimestamp)=> {
+    // Create a new Date object from the Unix timestamp
+    const date = new Date(unixTimestamp * 1000);
+  
+    // Get the day, month, and year components
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const year = date.getFullYear();
+  
+    // Format the date in ddmmyyyy format
+    return `${day}/${month}/${year}`;
+  }
 const Playerlist = {1:"ronaldo",2:"messi"}
 const Leaguelist = {}
 
