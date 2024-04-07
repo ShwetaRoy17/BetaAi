07b645ba19c7b10d254ad811a053f223d0c320b6
shweta roy <128317014+ShwetaRoy17@users.noreply.github.com>
2024-04-08 01:15:56 +0530

    routes redefined on frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Leagues from "./pages/leagues.jsx";
-import Root from "./layout.jsx";
-import Matches from "./pages/matches.jsx";
-import Main from "./pages/Main.jsx";
-import Home from "./pages/Home.jsx";
-import Profile from "./pages/Profile.jsx";
-import "./index.css";
 import store  from "./store/store.js";
 import { Provider } from "react-redux";
 import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from "react-router-dom";
-import League from "./components/League.jsx";
-import Team from "./components/Team.jsx";
-import Player from "./components/player.jsx";
+
+// pages import
+import Leagues from "./pages/leagues.jsx";
+import Root from "./layout.jsx";
+import Matches from "./pages/MatchesPage.jsx";
+import Main from "./pages/MatchPag.jsx";
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import "./index.css";
+import Team from "./pages/Team.jsx";
+import Player from "./pages/player.jsx";
+import LeagueStats from "./pages/League-stats.jsx";
+import LeagueMatches from "./pages/league-matches.jsx";
+import ErrorPage from "./pages/error-page.jsx"
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<Root />}>
-     <Route path='/matches/:matchid' element={<Main/>}></Route>
      <Route path='home' element={<Home/>}></Route>
       <Route path='leagues' element ={<Leagues/>}></Route>
       <Route path='matches' element ={<Matches/>}></Route>
       <Route path="profile" element={<Profile/>}></Route>
+      <Route path="league-stats/:season_id" element={<LeagueStats/>}></Route>
+      <Route path="match-stats/:season_id" element={<LeagueMatches/>}></Route>
       <Route path="search/team/:id" element={<Team/>}></Route>
-      <Route path="search/player/:id" element={<Player/>}></Route>
-      <Route path="search/league/:id" element={<League/>}></Route>
+      <Route path="search/player/:player_id" element={<Player/>}></Route>
+     <Route path='match/:matchId' element={<Main/>}></Route>
+     <Route path='*' element={<ErrorPage/>} />
 
   </Route>
   )
